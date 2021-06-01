d2ad76717fb85880a5956499e5002a0b9d0b1679
Renan Meneses <renan.meneses@hapvida.com.br>
2021-06-01 08:28:56 -0300

    Melhorando o about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -39,20 +39,58 @@ function About() {
            <strong className="purple">Skillset </strong> Professionnais
         </h1>
         <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+        <h1 className="project-heading" style = {{justifyContent: "center"}}>
+           <strong className="purple">Linguagens: </strong> 
+        </h1>
+        </Row>
+        <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+          {/* linguagem */}
           <Techstack iconName="devicon-java-plain-wordmark"/>
-          <Techstack iconName="devicon-cplusplus-line" />
+          <Techstack iconName="devicon-haskell-plain " />
           <Techstack iconName="devicon-python-plain-wordmark " />
+          <Techstack iconName="devicon-css3-plain-wordmark " />
+          <Techstack iconName="devicon-html5-plain-wordmark " />
+          <Techstack iconName="devicon-php-plain " />
+          <Techstack iconName="devicon-dart-plain-wordmark " />
+          <Techstack iconName="devicon-objectivec-plain " />
           <Techstack iconName="devicon-javascript-plain " />
           <Techstack iconName="devicon-typescript-plain"/>
+          <Techstack iconName="devicon-c-plain-wordmark"/>
+          <Techstack iconName="devicon-cplusplus-plain-wordmark"/>
+          <Techstack iconName="devicon-csharp-plain-wordmark"/>
+
+          </Row>
+          <h1 className="project-heading">
+           <strong className="purple">FrameWorks: </strong> 
+        </h1>
+        <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+            {/* FrameWorks */}
+          <Techstack iconName="devicon-spring-plain-wordmark"/>
+          
           <Techstack iconName="devicon-django-plain"/>
+          <Techstack iconName="devicon-flask-original-wordmark"/>
           <Techstack iconName="devicon-dot-net-plain-wordmark" />
-          <Techstack iconName="devicon-laravel-plain"/>
+          <Techstack iconName="devicon-laravel-plain-wordmark"/>
+          <Techstack iconName="devicon-swift-plain-wordmark"/>
+          <Techstack iconName="devicon-flutter-plain"/>
           <Techstack iconName="devicon-nodejs-plain-wordmark " />
           <Techstack iconName="devicon-express-original-wordmark" />
+          
           <Techstack iconName="devicon-react-original-wordmark" />
           <Techstack iconName="devicon-vuejs-plain-wordmark"/>
+          </Row>
+          <h1 className="project-heading">
+           <strong className="purple">Database: </strong> 
+        </h1>
+        <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+          {/* Bancos  */}
           <Techstack iconName="devicon-mongodb-plain-wordmark" />
-          <Techstack iconName="devicon-git-plain-wordmark" />
+          <Techstack iconName="devicon-mysql-plain-wordmark" />
+          <Techstack iconName="devicon-postgresql-plain-wordmark" />
+          <Techstack iconName="devicon-oracle-original" />
+          <Techstack iconName="devicon-firebase-plain-wordmark" />
+
+          
           <Techstack iconName="devicon-bootstrap-plain-wordmark" />
         </Row>
         
@@ -63,9 +101,14 @@ function About() {
           <Techstack iconName="devicon-linux-plain" />
           <Techstack iconName="cib-visual-studio-code" />
           <Techstack iconName="cib-postman" />
-          <Techstack iconName="cib-zeit" />
-          <Techstack iconName="cib-heroku" />
+          <Techstack iconName="devicon-heroku-plain-wordmark" />
+          <Techstack iconName="devicon-git-plain-wordmark" />
+          <Techstack iconName="devicon-gitlab-plain-wordmark" />
           <Techstack iconName="devicon-bitbucket-plain-wordmark" />
+          <Techstack iconName="devicon-docker-plain-wordmark" />
+          <Techstack iconName="devicon-composer-line-wordmark" />
+          <Techstack iconName="devicon-apache-plain-wordmark" />
+          <Techstack iconName="devicon-bash-plain" />
 
         </Row>
         <Github />
