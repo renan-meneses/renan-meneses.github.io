50cc015b7c142c29edc82d3e0aff673031c17bfd
Renan Meneses <renan.meneses@hapvida.com.br>
2021-06-02 07:41:11 -0300

    Treoca de imgens

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import myImg from "../../Assets/avatar.svg";
+import myImg from "../../Assets/avata.svg";
 import "../../style.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Tilt from "react-parallax-tilt";
